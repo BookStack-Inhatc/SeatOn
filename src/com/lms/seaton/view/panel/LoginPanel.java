22cb9ba50e4f1eeca58ee491a97304f01cc4fe19
parks <iso960@naver.com>
2025-12-14 04:01:22 +0900

    design: 로그인/회원가입/시작 화면 UI 패널 구성

--- src/com/lms/seaton/view/panel/LoginPanel.java
+++ src/com/lms/seaton/view/panel/LoginPanel.java
@@ -19,47 +19,132 @@ public class LoginPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        top = new javax.swing.JPanel();
+        btnGoBack = new javax.swing.JButton();
+        lblPw = new javax.swing.JLabel();
+        tbxNumber = new javax.swing.JTextField();
+        tbxPw = new javax.swing.JTextField();
+        BtnLogin = new javax.swing.JButton();
+        lblLogin = new javax.swing.JLabel();
+        lblNumber = new javax.swing.JLabel();
+        lblError = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
 
-        jLabel1.setText("LOGIN PANEL");
+        setPreferredSize(new java.awt.Dimension(500, 700));
 
-        jButton1.setText("뒤로가기");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jPanel1.setBackground(new java.awt.Color(0, 51, 102));
+        jPanel1.setPreferredSize(new java.awt.Dimension(500, 700));
+        jPanel1.setLayout(null);
+
+        top.setBackground(new java.awt.Color(255, 204, 51));
+
+        btnGoBack.setBackground(new java.awt.Color(255, 204, 0));
+        btnGoBack.setText("뒤로가기");
+        btnGoBack.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnGoBackActionPerformed(evt);
             }
         });
+
+        javax.swing.GroupLayout topLayout = new javax.swing.GroupLayout(top);
+        top.setLayout(topLayout);
+        topLayout.setHorizontalGroup(
+            topLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(topLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnGoBack)
+                .addContainerGap(416, Short.MAX_VALUE))
+        );
+        topLayout.setVerticalGroup(
+            topLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(topLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnGoBack)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel1.add(top);
+        top.setBounds(0, 0, 500, 40);
+
+        lblPw.setFont(new java.awt.Font("맑은 고딕", 1, 14)); // NOI18N
+        lblPw.setForeground(new java.awt.Color(255, 204, 0));
+        lblPw.setText("비밀번호 입력");
+        jPanel1.add(lblPw);
+        lblPw.setBounds(20, 600, 100, 17);
+        jPanel1.add(tbxNumber);
+        tbxNumber.setBounds(120, 560, 240, 23);
+        jPanel1.add(tbxPw);
+        tbxPw.setBounds(120, 600, 240, 23);
+
+        BtnLogin.setBackground(new java.awt.Color(255, 204, 0));
+        BtnLogin.setFont(new java.awt.Font("맑은 고딕", 1, 12)); // NOI18N
+        BtnLogin.setForeground(new java.awt.Color(0, 51, 102));
+        BtnLogin.setText("로그인");
+        jPanel1.add(BtnLogin);
+        BtnLogin.setBounds(370, 560, 100, 60);
+
+        lblLogin.setFont(new java.awt.Font("맑은 고딕", 1, 24)); // NOI18N
+        lblLogin.setForeground(new java.awt.Color(255, 204, 0));
+        lblLogin.setText("로그인");
+        jPanel1.add(lblLogin);
+        lblLogin.setBounds(220, 90, 72, 33);
+
+        lblNumber.setFont(new java.awt.Font("맑은 고딕", 1, 14)); // NOI18N
+        lblNumber.setForeground(new java.awt.Color(255, 204, 0));
+        lblNumber.setText("전화번호 입력");
+        jPanel1.add(lblNumber);
+        lblNumber.setBounds(20, 560, 100, 20);
+
+        lblError.setFont(new java.awt.Font("맑은 고딕", 1, 14)); // NOI18N
+        lblError.setForeground(new java.awt.Color(0, 51, 102));
+        lblError.setText("!ERROR!");
+        jPanel1.add(lblError);
+        lblError.setBounds(120, 630, 200, 20);
+
+        jSeparator1.setBackground(new java.awt.Color(255, 204, 0));
+        jSeparator1.setForeground(new java.awt.Color(255, 204, 0));
+        jPanel1.add(jSeparator1);
+        jSeparator1.setBounds(170, 140, 170, 10);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(159, 159, 159)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1)
-                    .addComponent(jLabel1))
-                .addContainerGap(163, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(112, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(93, 93, 93)
-                .addComponent(jButton1)
-                .addContainerGap(161, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
+
+        jPanel1.getAccessibleContext().setAccessibleName("");
+
+        getAccessibleContext().setAccessibleName("LOGIN");
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnGoBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGoBackActionPerformed
         frame.goBack();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnGoBackActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton BtnLogin;
+    private javax.swing.JButton btnGoBack;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblError;
+    private javax.swing.JLabel lblLogin;
+    private javax.swing.JLabel lblNumber;
+    private javax.swing.JLabel lblPw;
+    private javax.swing.JTextField tbxNumber;
+    private javax.swing.JTextField tbxPw;
+    private javax.swing.JPanel top;
     // End of variables declaration//GEN-END:variables
 }
--- src/com/lms/seaton/view/panel/StartPanel.java
+++ src/com/lms/seaton/view/panel/StartPanel.java
@@ -20,49 +20,85 @@ public class StartPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        btnLogin = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        top2 = new javax.swing.JPanel();
+        BtnLogin = new javax.swing.JButton();
+        lblLogin = new javax.swing.JLabel();
         btnSignUp = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
 
-        jLabel1.setText("START PANEL");
+        setPreferredSize(new java.awt.Dimension(500, 700));
 
-        btnLogin.setText("로그인");
-        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+        jPanel3.setBackground(new java.awt.Color(0, 51, 102));
+        jPanel3.setPreferredSize(new java.awt.Dimension(500, 700));
+        jPanel3.setLayout(null);
+
+        top2.setBackground(new java.awt.Color(255, 204, 51));
+
+        javax.swing.GroupLayout top2Layout = new javax.swing.GroupLayout(top2);
+        top2.setLayout(top2Layout);
+        top2Layout.setHorizontalGroup(
+            top2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 500, Short.MAX_VALUE)
+        );
+        top2Layout.setVerticalGroup(
+            top2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jPanel3.add(top2);
+        top2.setBounds(0, 0, 500, 40);
+
+        BtnLogin.setBackground(new java.awt.Color(255, 204, 0));
+        BtnLogin.setFont(new java.awt.Font("맑은 고딕", 1, 12)); // NOI18N
+        BtnLogin.setForeground(new java.awt.Color(0, 51, 102));
+        BtnLogin.setText("로그인");
+        BtnLogin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnLoginActionPerformed(evt);
+                BtnLoginActionPerformed(evt);
             }
         });
+        jPanel3.add(BtnLogin);
+        BtnLogin.setBounds(120, 350, 130, 190);
 
+        lblLogin.setFont(new java.awt.Font("맑은 고딕", 1, 24)); // NOI18N
+        lblLogin.setForeground(new java.awt.Color(255, 204, 0));
+        lblLogin.setText("환영합니다!");
+        jPanel3.add(lblLogin);
+        lblLogin.setBounds(190, 90, 130, 33);
+
+        btnSignUp.setBackground(new java.awt.Color(255, 204, 0));
+        btnSignUp.setFont(new java.awt.Font("맑은 고딕", 1, 12)); // NOI18N
+        btnSignUp.setForeground(new java.awt.Color(0, 51, 102));
         btnSignUp.setText("회원가입");
         btnSignUp.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnSignUpActionPerformed(evt);
             }
         });
+        jPanel3.add(btnSignUp);
+        btnSignUp.setBounds(270, 350, 130, 190);
+
+        jSeparator1.setBackground(new java.awt.Color(255, 204, 0));
+        jSeparator1.setForeground(new java.awt.Color(255, 204, 0));
+        jPanel3.add(jSeparator1);
+        jSeparator1.setBounds(170, 140, 170, 10);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(163, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnSignUp)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(btnLogin)
-                        .addComponent(jLabel1)))
-                .addGap(159, 159, 159))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(156, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(jLabel1)
-                .addGap(52, 52, 52)
-                .addComponent(btnLogin)
-                .addGap(28, 28, 28)
-                .addComponent(btnSignUp)
-                .addContainerGap(143, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(42, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -70,15 +106,24 @@ public class StartPanel extends javax.swing.JPanel {
         frame.showCard("SIGNUP");
     }//GEN-LAST:event_btnSignUpActionPerformed
 
-    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+    private void BtnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnLoginActionPerformed
         frame.showCard("LOGIN");
-    }//GEN-LAST:event_btnLoginActionPerformed
+    }//GEN-LAST:event_BtnLoginActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnLogin;
+    private javax.swing.JButton BtnLogin;
+    private javax.swing.JButton btnGoBack;
+    private javax.swing.JButton btnGoBack1;
     private javax.swing.JButton btnSignUp;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblLogin;
+    private javax.swing.JPanel top;
+    private javax.swing.JPanel top1;
+    private javax.swing.JPanel top2;
     // End of variables declaration//GEN-END:variables
 
 }
